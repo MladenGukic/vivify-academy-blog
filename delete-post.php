b6f64a03b9e7f9b6eb4cae5be43549c2d1850c57
MladenGukic <mladengukic@gmail.com>
2019-08-02 23:58:48 +0200

    Finished project

--- delete-post.php
+++ delete-post.php
@@ -3,6 +3,11 @@ include "db.php";
 
 $id = $_GET['id'];
 
+$sqlDelCom = "DELETE FROM comments WHERE post_id = $id";
+$statementDelCom = $connection->prepare($sqlDelCom);
+$statementDelCom->execute();
+$statementDelCom->setFetchMode(PDO::FETCH_ASSOC);
+
 $sqlDel = "DELETE FROM posts WHERE id = $id";
 $statementDelete = $connection->prepare($sqlDel);
 $statementDelete->execute();
